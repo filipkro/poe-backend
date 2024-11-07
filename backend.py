a5df0a90f1181a80262adb5a5658a2ff343e3984
filipkro <filip.kronstrom@gmail.com>
2024-11-07 20:33:31 +0100

    cleanup

--- backend.py
+++ backend.py
@@ -1,10 +1,5 @@
-from flask import Flask, request, send_file
-import flask
+from flask import Flask, request
 import os
-import json
-from werkzeug.utils import secure_filename
-import boto3
-from configparser import ConfigParser
 import backend_utils
 
 app = Flask(__name__)
@@ -27,6 +22,7 @@ def get_ongoing():
         Returns 'Ongoing' if assessment is ongoing and 'Finished' if it is
         completed.
     '''
+    raise NotImplementedError("Ongoing is not implemented yet")
     id = backend_utils.get_variable_from_req(request, 'id')
     print(f'Check ongoing for user with id {id}')
     if id is None:
@@ -54,134 +50,11 @@ def analyse_video():
     return f"{path} to be analysed,\n{status}", 200
 
 
-@app.route('/tt', methods=['POST'])
-def tt():
-    print('writing file??')
-    with open('/data/out.txt', 'w') as fo:
-        fo.write('lol')
-
-    return "ok", 200
-
-@app.route('/upload', methods=['POST'])
-def upload_video():
-    '''
-        Upload video to assess. Specify id and leg
-        (and frames between repetitions, TO BE IMPLEMENTED).
-        Video provided as file in files.
-    '''
-    import cv2
-    id = backend_utils.get_variable_from_req(request, 'id')
-    print(f'Upload for user with id {id}')
-    if id is None:
-        print("No id provided")
-        return "No id provided", 400
-    leg = backend_utils.get_variable_from_req(request, 'leg')
-    if leg is None:
-        print("No leg provided")
-        return "No leg provided", 400
-
-    debug = backend_utils.get_variable_from_req(request, 'debug')
-    print(f'DEBUG: {debug}')
-
-    if not backend_utils.check_user_exist(id):
-        print("User not in database")
-        return "User not in database", 404
-
-    attempt = backend_utils.get_attempt_nbr(id)
-
-    frame_splits = request.form.getlist('frames')
-    print(id)
-
-    meta = {'leg': leg, 'frames': tuple(frame_splits)}
-
-    meta_name = 'meta'
-    f = open(meta_name, 'w')
-    json.dump(meta, f)
-    f.close()
-
-    s3_path = f'users/{id}/ATTEMPT{attempt}/meta.json'
-    uploaded = backend_utils.upload_to_aws(meta_name, s3_path)
-
-    if not uploaded:
-        print("Could not save meta data to S3")
-        return "Could not save meta data to S3", 501
-
-    if 'file' not in request.files:
-        print(request.url)
-        print("no file in files")
-        return "No file part", 400
-    file = request.files['file']
-    if file.filename == '':
-        print("no filename")
-        return "No video selected for uploading", 400
-    else:
-        filename = secure_filename(file.filename)
-        print(filename)
-
-        file_path = os.path.join('/app', filename)
-        file.save(file_path)
-        print(type(file))
-        file.close()
-        # cap = cv2.VideoCapture(file_path)
-        with open(file_path, 'rb') as fi:
-            file = fi.read()
-        s3_name = f'users/{id}/ATTEMPT{attempt}/vid.'
-        s3_name = s3_name + filename.split('.')[-1] if '.' in filename else \
-            s3_name + 'mp4'
-        print(s3_name)
-        uploaded = backend_utils.upload_to_aws(file_path, s3_name)
-        os.remove(file_path)
-        if uploaded:
-            print('upload_video filename: ' + filename)
-            status = backend_utils.predict(s3_name, id, leg, file, attempt=attempt,
-                                           debug=debug)
-            return f"{filename} uploaded,\n{status}", 200
-        else:
-            print("not uploaded to aws correctly")
-            return "File could not be uploaded to S3", 501
-
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!\nLOL1</p>"
 
-
-@app.route("/create_user", methods=["POST"])
-def create_user():
-    '''
-        Create user in database, provide id, injured leg, weight, and length.
-    '''
-    id = backend_utils.get_variable_from_req(request, 'id')
-    print(f'Creating user with id {id}')
-    if id is None or id == '':
-        return "No id provided", 400
-    leg = request.form.get('leg')
-
-    weight = request.form.get('weight')
-    length = request.form.get('length')
-    name = request.form.get('name')
-    injury = request.form.get('injury')
-    sex = request.form.get('sex')
-    date = request.form.get('date')
-
-    if backend_utils.check_user_exist(id):
-        # overwrite previous information??
-        return "User already exists", 401
-
-    params = {'id': id, 'leg': leg, 'weight': weight, 'length': length,
-              'name': name, 'injury': injury, 'sex': sex, 'date': date}
-
-    f = open('user_params', 'w')
-    json.dump(params, f)
-    f.close()
-
-    s3_path = f'users/{id}/user_params.json'
-    uploaded = backend_utils.upload_to_aws('user_params', s3_path)
-
-    if uploaded:
-        return "User successfully created", 200
-    else:
-        return "Something went wrong when saving to S3, plz view logs", 501
 
 
 if __name__ == '__main__':
--- backend_utils.py
+++ backend_utils.py
@@ -1,12 +1,11 @@
 import os
-import boto3
+# import boto3
 #from configparser import ConfigParser
-from botocore.exceptions import NoCredentialsError, ClientError
-from dateutil import tz
-from datetime import date
-from numpy import argmax
-import re
-import datetime
+# from botocore.exceptions import NoCredentialsError, ClientError
+# from dateutil import tz
+# from datetime import date
+# import re
+# import datetime
 from rq import Queue
 import redis
 import pipeline
@@ -41,74 +40,6 @@ def get_variable_from_req(request, key):
     return var
 
 
-def get_results(id, attempt, with_reps=False):
-    id = '950203'
-    attempt = 1
-    # file = 'result.pkl'
-    file = 'dummy-data/950203/ATTEMPT1/result.pkl'
-    s3_file = f'users/{id}/ATTEMPT{attempt}/results.pkl'
-    downloaded, error = download_from_aws(file, s3_file)
-
-    if downloaded:
-        import pickle
-
-        f = open(file, 'rb')
-        results = pickle.load(f)
-        f.close()
-
-        pred = {}
-        conf = {}
-        reps = {}
-        for poe in results:
-            pred[poe] = results[poe]['pred']
-            conf[poe] = tuple(results[poe]['conf'])
-            if with_reps:
-                individual = results[poe]['detailed']
-                reps[poe] = {'pred': [], 'conf': []}
-                for i, rep in enumerate(individual):
-                    reps[poe]['pred'].append(int(argmax(rep)))
-                    reps[poe]['conf'].append(tuple(rep))
-                # reps[poe] = {}
-                # for i, rep in enumerate(individual):
-                #     reps[poe][f'rep{i}'] = int(argmax(rep))
-                #     reps[poe][f'rep{i}confs'] = tuple(rep)
-
-        vid = f'users/{id}/ATTEMPT{attempt}/vid.mts'
-        utc = get_modified_time(vid)
-
-        if with_reps:
-            return {'combined': {'time': str(utc), 'pred': pred, 'conf': conf},
-                    'reps': reps}
-        else:
-            return {'time': str(utc), 'pred': pred, 'conf': conf}
-
-    return error
-
-
-def fix_id(id):
-    id = re.sub('[^0-9]', '', str(id))
-    current = datetime.date.today().year
-    if not (id[0] == '1' or id[0] == '2'):
-        if len(id) == 6 or len(id) == 10:
-            year = int(id[:2])
-            id = '20' + id if year < current % 100 else '19' + id
-        else:
-            return id, 'Wrong id'
-    if len(id) == 8:
-        id = id + 'XXXX'
-        return id, 'Wrong id, add four digits'
-
-    return id, ''
-
-
-# def get_attempt_nbr(id):
-#     nbr = 1
-#     base = f'users/{id}/ATTEMPT'
-
-#     while file_on_aws(base + str(nbr)):
-#         nbr += 1
-
-#     return nbr
 def get_attempt_nbr(id):
 
     return 2
@@ -132,63 +63,6 @@ def check_ongoing(id, attempt):
 
 def check_result_available(id, attempt):
     return file_on_aws(f'users/{id}/ATTEMPT{attempt}/results.pkl')
-
-
-def get_modified_time(file):
-    return date.today()
-# def get_modified_time(file):
-#     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-#                       aws_secret_access_key=SECRET_KEY)
-
-#     to_zone = tz.gettz('Europe/Stockholm')
-#     utc = s3.list_objects(Bucket='poe-uploads',
-#                           Prefix=file)['Contents'][0]['LastModified']
-#     return utc.astimezone(to_zone)
-
-def upload_to_aws(local_file, s3_file):
-    print("Upload Successful")
-    return True
-# def upload_to_aws(local_file, s3_file):
-#     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-#                       aws_secret_access_key=SECRET_KEY)
-
-#     bucket = 'poe-uploads'
-#     try:
-#         s3.upload_file(local_file, bucket, s3_file)
-#         print("Upload Successful")
-#         return True
-#     except FileNotFoundError:
-#         print("The file was not found")
-#         return False
-#     except NoCredentialsError:
-#         print("Credentials not available")
-#         return False
-#     except ClientError:
-#         print("ClientError")
-#         return False
-
-
-def download_from_aws(local_file, s3_file):
-    return True, 'Download Successful'
-# def download_from_aws(local_file, s3_file):
-#     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-#                       aws_secret_access_key=SECRET_KEY)
-
-#     bucket = 'poe-uploads'
-#     try:
-#         s3.download_file(bucket, s3_file, local_file)
-#         del s3
-#         print("Download Successful")
-#         return True, 'Download Successful'
-#     except FileNotFoundError:
-#         print("The file was not found")
-#         return False, 'The file was not found'
-#     except NoCredentialsError:
-#         print("Credentials not available")
-#         return False, 'Credentials not available'
-#     except ClientError:
-#         print("ClientError")
-#         return False, 'ClientError'
 
 def delete_from_aws(file):
     print("Delete Successful")
